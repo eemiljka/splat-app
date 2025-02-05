d2e811aee52e075188e54406a780dc6e688fe344
eemiljka <eemiljka@metropolia.fi>
2025-02-05 12:29:40 +0200

    deleted sphere, added person, added a room plant

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ const createScene = function() {
   const scene = new Scene(engine);
 
   // Create an ArcRotateCamera
-  const camera = new ArcRotateCamera('camera1', Math.PI / 2, 1, 10, new Vector3(0, 0, 0), scene);
+  const camera = new ArcRotateCamera('camera1', 5, 1, 10, new Vector3(0, 3, 0), scene);
   
   // Attach the camera to the canvas
   camera.attachControl(canvas, false);
@@ -21,17 +21,25 @@ const createScene = function() {
   const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene);
   
   // Create a built-in "sphere" shape using the SphereBuilder
-  const sphere = MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: Mesh.FRONTSIDE}, scene);
+  //const sphere = MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 0.2, sideOrientation: Mesh.FRONTSIDE}, scene);
 
   // Move the sphere upward 1/2 of its height
-  sphere.position.y = 1;
+  //sphere.position.y = 1;
 
   // Create a built-in "ground" shape;
   const ground = MeshBuilder.CreateGround('ground1', {width: 6, height: 6, subdivisions: 2, updatable: false}, scene);
 
-  SceneLoader.ImportMeshAsync('splat', './', 'cls2.splat', scene).then((result) => {
+  SceneLoader.ImportMeshAsync('splat', './', 'pesron2.splat', scene).then((result) => {
     const splat = result.meshes[0];
-    splat.position = new Vector3(0, 0, 0);
+    splat.position = new Vector3(-1, 4.7, 0);
+  });
+
+  SceneLoader.ImportMeshAsync(null, './', 'house_plant.glb', scene).then((result) => {
+    const glb = result.meshes[0];
+    glb.position = new Vector3(2.5, 0, 2);
+    glb.scaling = new Vector3(0.60, 0.60, 0.60)
+  }).catch((error) => {
+    console.error('Error loading room plant:', error);
   });
 
   // Return the created scene
